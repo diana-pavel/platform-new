54909b22c0113dfed00702b088296b5d255e3ccc
Diana Pavel <dianapav@gmail.com>
2019-11-27 15:50:11 +0000

    created the recruiters table in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_27_154154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recruiters", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "surname"
+    t.string "company"
+    t.string "phone"
+    t.string "profile_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_recruiters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true
+  end
 
 end
